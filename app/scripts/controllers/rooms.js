7bebbad488c2993b13bed5e848cb41d819464eba
Gurmeet <happyteched@gmail.com>
2014-01-18 19:14:00 +0530

    adding room related files

---
+++ app/scripts/controllers/rooms.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('snapmeetingApp')
+  .controller('RoomsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
